1b0690431019c470686f3719e066944250cb7a15
komali2 <rogersjcaleb@gmail.com>
2016-09-10 17:36:54 -0700

    dev: code cleanup

--- server/havenController.js
+++ server/havenController.js
@@ -7,12 +7,12 @@ api.analyseSentiment = function(str, cb){
   var options = {
     method: 'GET',
     url: `	https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?apikey=${key}&text=${str}`
-  }
+  };
 
   request(options, (error, response, body)=>{
     if(error){
 
     }
     cb(body);
-  })
-}
+  });
+};
