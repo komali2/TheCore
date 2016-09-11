067d4e77e4f21bbe0a1a29f9fcf1d84cb23c2478
komali2 <rogersjcaleb@gmail.com>
2016-09-10 17:36:02 -0700

    bug: wrong syntax

--- server/twitterController.js
+++ server/twitterController.js
@@ -1,11 +1,11 @@
 module.exports = {
-  getUserTweets = function(username){
+  getUserTweets: function(username){
     var listOfTweets = [];
 
     return listOfTweets;
-  }
+  },
 
-  stringifyAllTweets = function(tweets){
+  stringifyAllTweets: function(tweets){
     var str = '';
     tweets.forEach((tweet)=>{
       str = str + ' ' + tweet.text;
